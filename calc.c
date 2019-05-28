d66d44df3f3f7d714521af4beecf4e82b4f73553
car4ryu <car.ryu.ryu.ryu@gmail.com>
2019-05-28 15:34:03 +0900

    [add]added initial C program for git practice

---
+++ calc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int a, b, res = 0;
+	printf("this program will add two numbers\n");
+	printf("input your first number here > ");
+	scanf("%d", &a);
+	printf("input your second number here > ");
+	scanf("%d", &b);
+
+	res = a + b;
+
+	printf("the result is : %d\n", res);
+
+}
+
